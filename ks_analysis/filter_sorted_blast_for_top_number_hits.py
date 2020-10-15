3cd8f27125531199674e4b3fa8b8cc22980dfe4e
Liming Cai <daybreak.chua@gmail.com>
2020-10-15 00:07:10 -0700

    Update filter_sorted_blast_for_top_number_hits.py

--- ks_analysis/filter_sorted_blast_for_top_number_hits.py
+++ ks_analysis/filter_sorted_blast_for_top_number_hits.py
@@ -3,12 +3,13 @@ import sys
 x=open(sys.argv[1]).readlines()
 y=open(sys.argv[1]+'.top','a')
 cur_rec=''
-#i=0
+i=0
 for l in x:
 	if l.split()[0]!=cur_rec:
 		cur_rec=l.split()[0]
+		i=0
 		if l.split()[0]!=l.split()[1]:
-			i=1
+			i=i+1
 			y.write(l)
 	else:
 		if i<int(sys.argv[2]):
